d1bc37a94ed187b9b9dd413f7a63f93f27fd0644
Yeasir Arafat Shahed <arafatshahed@users.noreply.github.com>
2020-06-13 15:13:59 +0600

    Update app.js

--- app.js
+++ app.js
@@ -33,19 +33,49 @@ async function getCovidApi() {
     "https://corona.lmao.ninja/v2/countries/Bangladesh"
   );
   const jsFormatData = await jsonFormatData.json();
-  document.getElementById("nc").innerHTML = jsFormatData.todayCases;
-  document.getElementById("nd").innerHTML = jsFormatData.todayDeaths;
-  document.getElementById("nrc").innerHTML = jsFormatData.todayRecovered;
-  document.getElementById("tc").innerHTML = jsFormatData.cases;
-  document.getElementById("td").innerHTML = jsFormatData.deaths;
-  document.getElementById("tr").innerHTML = jsFormatData.recovered;
-  document.getElementById("tst").innerHTML = jsFormatData.tests;
-  const date = new Date(parseInt(jsFormatData.updated));
-  const lastupdated = date.toString();
+  if (jsFormatData.todayCases == 0 && jsFormatData.todayDeaths == 0) {
+    const jsonFormatData2 = await fetch("https://api.covid19api.com/summary");
+    const jsFormatData2 = await jsonFormatData2.json();
+    document.getElementById("nc").innerHTML =
+      jsFormatData2.Countries[13].NewConfirmed;
+    document.getElementById("nd").innerHTML =
+      jsFormatData2.Countries[13].NewDeaths;
+    document.getElementById("nrc").innerHTML =
+      jsFormatData2.Countries[13].NewRecovered;
+    document.getElementById("tc").innerHTML =
+      jsFormatData2.Countries[13].TotalConfirmed;
+    document.getElementById("td").innerHTML =
+      jsFormatData2.Countries[13].TotalDeaths;
+    document.getElementById("tr").innerHTML =
+      jsFormatData2.Countries[13].TotalRecovered;
+    document.getElementById("tst").innerHTML = jsFormatData.tests;
+    const date = jsFormatData2.Countries[13].Date;
+    const lastupdated = Date.parse(date);
+    const date2 = new Date(parseInt(lastupdated));
+    const lastupdated2 = date2.toString();
 
-  const t = formatDate(lastupdated);
-  var s = t.replace("GMT+0600 (Bangladesh Standard Time)", "");
-  console.log(formatDate(lastupdated));
-  document.getElementById("update").innerHTML = s;
+    const t = formatDate(lastupdated2);
+    var s = t.replace("GMT+0600 (Bangladesh Standard Time)", "");
+    document.getElementById("update").innerHTML = s;
+    console.log(s);
+    console.log("Result Coming from late API");
+  } else {
+    document.getElementById("nc").innerHTML = jsFormatData.todayCases;
+    document.getElementById("nd").innerHTML = jsFormatData.todayDeaths;
+    document.getElementById("nrc").innerHTML = jsFormatData.todayRecovered;
+    document.getElementById("tc").innerHTML = jsFormatData.cases;
+    document.getElementById("td").innerHTML = jsFormatData.deaths;
+    document.getElementById("tr").innerHTML = jsFormatData.recovered;
+    document.getElementById("tst").innerHTML = jsFormatData.tests;
+    const date = new Date(parseInt(jsFormatData.updated));
+    const lastupdated = date.toString();
+
+    const t = formatDate(lastupdated);
+    var s = t.replace("GMT+0600 (Bangladesh Standard Time)", "");
+    console.log(formatDate(lastupdated));
+    document.getElementById("update").innerHTML = s;
+    console.log(s);
+    console.log("Result Coming from fast API");
+  }
 }
 getCovidApi();
